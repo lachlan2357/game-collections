148bd9dfcde651a2b1b547e174a61777541776a1
lachlan2357 <33368366+lachlan2357@users.noreply.github.com>
2023-05-04 09:58:46 +0000

    Update gh-pages to output generated at a33e498

--- src/logic/encoder.ts
+++
@@ -1,2 +0,0 @@
-export const encode = (array: number[]) => array.map(num => num.toString(36)).join("-");
-export const decode = (string: string) => string.split("-").map(base36 => parseInt(base36, 36));
